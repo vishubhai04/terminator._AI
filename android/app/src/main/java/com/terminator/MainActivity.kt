e4c87df0fa7000108a85aed383f3a55c3521bc5b
vishubhai04 <134777620+vishubhai04@users.noreply.github.com>
2025-07-14 15:16:57 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/terminator/MainActivity.kt
+++ android/app/src/main/java/com/terminator/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.terminator
+package com.vishubhai04.terminator_ai
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
